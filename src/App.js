8a8c7f125d3e4215b3c819384703645a08fcf0bd
Ritik Jain <iritikjain@gmail.com>
2023-04-15 17:02:59 +0530

    Merge 2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import DefaultLayout from './layouts/Default.Layout';
 import SignIn from './components/SignIn/SignIn.Component';
 import SignUp from './components/SignUp/SignUp.Component';
 import NGORegistration from './components/NGORegistration/NGORegistration.Component'
+import AllCourses from './pages/AllCourses.Page'
+import Dashboard from './pages/Dashboard.Page'
+import SingleCourse from './pages/SingleCourse.Page';
+import CoursePlayPage from './pages/CoursePlay.Page';
+import CartPage from './pages/Cart.Page';
 // import axios from 'axios';
 
 // Import css files
@@ -27,6 +32,51 @@ function App() {
         <Route path='/login' exact element={<SignIn />} />
         <Route path='/register' exact element={<SignUp />} />
         <Route path='/ngoregistration' exact element={<NGORegistration />} />
+        <Route
+          path='/dashboard'
+          exact
+          element={
+            <DefaultLayout>
+              <Dashboard />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path='/courses'
+          exact
+          element={
+            <DefaultLayout>
+              <AllCourses />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path='/course/:id'
+          exact
+          element={
+            <DefaultLayout>
+              <SingleCourse />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path='/course/play/:id'
+          exact
+          element={
+            <DefaultLayout>
+              <CoursePlayPage />
+            </DefaultLayout>
+          }
+        />
+        <Route
+          path='/cart'
+          exact
+          element={
+            <DefaultLayout>
+              <CartPage />
+            </DefaultLayout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
