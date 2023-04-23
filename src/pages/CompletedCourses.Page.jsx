d16504f5d0379e23b2757e9e94723cbc2dc7db85
Harsh Gupta <harshgupta110101@gmail.com>
2023-04-23 14:26:15 +0530

    Minor: Integrated Completed Course

--- src/pages/CompletedCourses.Page.jsx
+++ src/pages/CompletedCourses.Page.jsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import Sidebar from '../components/Sidebar/Sidebar.Component';
 import CompletedCourses from '../components/CompletedCourses/CompletedCourses.Component';
-import courses from '../utils/data';
+// import courses from '../utils/data';
 
 const CompletedCoursesPage = () => {
+  const [courseDetails, setCourseDetails] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      await fetch('http://127.0.0.1:5000/course/course-completed', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: String(localStorage.getItem('token')),
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // handle successful response
+          if (!data.status) {
+            throw new Error(data.message);
+          }
+          console.log(data.courses)
+          setCourseDetails(data.courses);
+        })
+        .catch((error) => {
+          // handle error response
+          console.log(error);
+        });
+    };
+    getData();
+  }, []);
 
   return (
     <>
@@ -17,7 +44,7 @@ const CompletedCoursesPage = () => {
           <div className='lg:w-4/5 p-4 bg-white rounded'>
             <h1 className='text-2xl font-bold my-3'>Completed Courses</h1>
             <CompletedCourses 
-            posters={courses}
+            posters={courseDetails}
           />
           </div>
         </div>
